1eaf8d83b0a13cbc848dcfd0baa13d32a2c472ed
Lildam254 <163730699+Lildam254@users.noreply.github.com>
2024-03-17 14:54:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
+  ['254798482810', 'lildam', true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
